de3af0d2eed33b218aa569811c83252e2618306e
hemant1990 <hemantdevre@gmail.com>
2019-07-14 10:51:08 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -9,6 +9,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    App a=new App();
+        a.fun1();
         System.out.println( "From sacred games" );
     }
+    
+pulblic void fun1()
+{
+    System.out.println("Here you go with fun1")
+    
 }
+
+}
+
